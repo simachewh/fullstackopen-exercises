a3f4cb7d0536ca3c6ada3cab2104040c94409a23
Simachew Tibebu <stibebu@avain.intra>
2020-01-16 15:33:22 +0200

    correct a spelling error

--- course/src/components/content.js
+++ course/src/components/content.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Content = ( params ) => {
     return (
         <>
-            <p>{params.part1} {params.exercise1}</p>
-            <p>{params.part2} {params.exercise2}</p>
-            <p>{params.part3} {params.exercise3}</p>
+            <p>{params.part1} {params.exercises1}</p>
+            <p>{params.part2} {params.exercises2}</p>
+            <p>{params.part3} {params.exercises3}</p>
         </>
     )
 }
