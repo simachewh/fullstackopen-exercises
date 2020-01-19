1e656b611625f06e3f844ccedd6ecba51cfb3d9f
Simachew Tibebu <stibebu@avain.intra>
2020-01-19 22:41:49 +0200

    vscode in gitignore, soln to 1.8 unicafe

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,26 +1,67 @@
-import React, { useState } from 'react'
+import React, {
+    useState
+} from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0);
+    
+    const increamentTotal = () => {
+        setTotal(total + 1);
+    }
 
-  return (
-    <div>
-      <h1>Give Feedback</h1>
-      <button onClick={()=> { setGood( good + 1 ) } }>Good</button>
-      <button onClick={()=> { setNeutral( neutral + 1 ) } }>Neutral</button>
-      <button onClick={()=> setBad( bad + 1 ) }>Bad</button>
-      <h2>Statistics</h2>
-    <p>Goood: {good}</p>
-    <p>Neutral: {neutral}</p>
-  <p>Bad: {bad}</p>
-    </div>
-  )
+    const increamentGood = () => {
+        setGood(good + 1);
+        increamentTotal();
+    }
+
+    const increamentNeutral = () => {
+        setNeutral(neutral + 1);
+        increamentTotal();
+    }
+
+    const increamentBad = () => {
+        setBad(bad + 1);
+        increamentTotal();
+    }
+
+    const calculateAverage = () => {
+        if( total == 0 )
+        {
+            return 0;
+        }
+        return ( good * 1 + bad * -1 + neutral * 0 ) / total;
+    }
+
+    const calculatePositivePercentage = () => {
+        if (total == 0) {
+            return 0;
+        }
+        return ( good / total ) * 100;
+    }
+
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <button onClick={increamentGood}>Good</button>
+            <button onClick={increamentNeutral}>Neutral</button>
+            <button onClick={increamentBad}>Bad</button>
+            <h2>Statistics</h2>
+            <p>Goood: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>All: {total}</p>
+            <p>Average: {calculateAverage()}</p>
+            <p>Positive: {calculatePositivePercentage()} %</p>
+        </div>
+      );
+
+
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+ReactDOM.render( < App / > ,
+    document.getElementById('root')
 )
