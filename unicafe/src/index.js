e1364c0e64c318e7865f144e449602c0c4044411
Simachew Tibebu <stibebu@avain.intra>
2020-01-24 17:42:46 +0200

    soln for 1.10 unicafe step 5

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => {
+    return(
+        <div>
+            <button onClick={props.clickHandler}>{props.label}</button>
+        </div>
+    );
+}
+
+const Statistic = (props) => {
+    return (
+<p>{props.text}: {props.value} {props.unit}</p>
+    );
+}
+
 const Statistics = ( props ) => {
     if( props.total == 0 )
     {
@@ -14,12 +28,13 @@ const Statistics = ( props ) => {
     return (
         <div>
             <h2>Statistics</h2>
-            <p>Goood: {props.good}</p>
-            <p>Neutral: {props.neutral}</p>
-            <p>Bad: {props.bad}</p>
-            <p>All: {props.total}</p>
-            <p>Average: {props.average()}</p>
-            <p>Positive: {props.positive()} %</p>
+            <p>Good: {props.good}</p>
+            <Statistic text="Good" value={props.good}/>
+            <Statistic text="Neutral" value={props.neutral}/>
+            <Statistic text="Bad" value={props.bad}/>
+            <Statistic text="All" value={props.total}/>
+            <Statistic text="Average" value={props.average()}/>
+            <Statistic text="Positive" value={props.positive()} unit="%" />
         </div>
     );
 }
@@ -67,9 +82,9 @@ const App = () => {
     return (
         <div>
             <h1>Give Feedback</h1>
-            <button onClick={increamentGood}>Good</button>
-            <button onClick={increamentNeutral}>Neutral</button>
-            <button onClick={increamentBad}>Bad</button>
+            <Button clickHandler={increamentGood} label="Good" />
+            <Button clickHandler={increamentNeutral} label="Neutral"/>
+            <Button clickHandler={increamentBad} label="Bad"/>
             <Statistics good={good} 
                 bad={bad} 
                 neutral={neutral}
